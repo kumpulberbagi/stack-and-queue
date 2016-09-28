83d13964c75add12a3f92b382f9fe578fe647b39
Izhharuddin Syanmil <email": null>
2016-09-28 19:54:00 +0700

    stack done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # stack-and-queue
+stack berguna untuk melakukan perhitungan first in last out, dimana berguna untuk membuat data dasar tentang informasi orang atau masalah terdekat yang harus diselesaikan atau yang informasi dan datanya masih fresh sehingga lebih mudah diurai
+Queue atau first in first out, dimana informasi realtime terkait sesuatu.
+linked untuk memperkecil proses yang terjadi sehingga lebih mudah terproses
--- stack.js
+++ stack.js
@@ -2,18 +2,54 @@
 
 class Stack {
   constructor() {
+    this.maxsize = 20;
+    this.top = 0;
+    this.item = []
   }
 
   push(x) {
+    if (this.top === this.maxsize) {
+      return "penuh"
+    }
+    this.top = this.top + 1
+    return this.item.push(x);
   }
 
   pop() {
+    if (this.top === 0){
+      return "sudah kosong"
+    }
+    this.top = this.top - 1
+    return this.item.pop();
   }
 
   peek() {
-
+    if (this.top === 0){
+      return "item kosong"
+    }
+    return this.item[(this.top)-1]
   }
 
   isEmpty() {
+    if (this.top === 0){
+      return "kosong bro"
+    } else {
+      return "isi kok masih"
+    }
   }
 }
+
+let my_stack = new Stack()
+
+console.log(my_stack.isEmpty())
+my_stack.push("JavaScript")
+my_stack.push("is just so")
+my_stack.push("cool")
+my_stack.push(15)
+console.log(my_stack.peek())
+console.log(my_stack.pop());
+console.log(my_stack.pop());
+console.log(my_stack.pop());
+console.log(my_stack.pop());
+console.log(my_stack.isEmpty())
+console.log(my_stack.pop());
