ad31081a0341075be8a0ffcd4d186d1965e7d366
Alessandro C <alessandro.christopher@gmail.com>
2016-09-28 15:49:18 +0700

    Submitted

--- queue.js
+++ queue.js
@@ -1,5 +1,46 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor(a = []) {
+    this._a = a
+  }
+
+  push(x) {
+    this._a.push(x)
+    return this
+  }
+
+  shift() {
+    this._a.shift()
+    return this
+  }
+
+  peek() {
+    this._a.reverse().peek()
+    return this.reverse()
+
+  }
+
+  isEmpty() {
+    if (this._a.length === 0)
+      return true
+    else {
+      return false
+    }
+  }
 }
+  let myQueue = new Queue()
+  myQueue.push("Javascript")
+  myQueue.push("is just so")
+  myQueue.push("cool")
+  myQueue.push("a")
+
+
+  console.log(myQueue.shift())
+  console.log(myQueue.shift())
+  console.log(myQueue.shift())
+  console.log(myQueue.shift())
+
+  console.log(`The stack is empty? ${myQueue.isEmpty()}`)
+
+  console.log(myQueue.shift())
--- stack.js
+++ stack.js
@@ -1,19 +1,57 @@
 "use strict"
-
 class Stack {
-  constructor() {
-  }
+  constructor(a = []) {
+    this._a = a
 
+  }
+  isFull(){
+    if (this._a.length === 4){
+       return true
+    }
+    else {
+      return false
+    }
+  }
   push(x) {
+    this._a.push(x)
+    return this
   }
 
   pop() {
+    this._a.pop()
+    return this
   }
 
   peek() {
+    this._a.peek()
+    return this
 
   }
 
   isEmpty() {
+    if (this._a.length === 0)
+      return true
+    else {
+      return false
+    }
   }
 }
+
+let myStack = new Stack()
+myStack.push("Javascript")
+myStack.push("is just so")
+myStack.push("cool")
+myStack.push("a")
+
+console.log(`The stack is Full? ${myStack.isFull()}`)
+console.log(myStack.pop())
+console.log(myStack.pop())
+console.log(myStack.pop())
+console.log(myStack.pop())
+console.log(myStack.pop())
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+
+
+console.log(myStack.pop())
+console.log("---",myStack)
