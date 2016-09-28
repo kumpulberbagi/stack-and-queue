65ac14c9cea0c82d697ce59be105d6677eef29aa
tonychen09 <tony_chen93@yahoo.com>
2016-09-28 14:21:50 +0700

    stack

--- queue.js
+++ queue.js
@@ -1,5 +1,53 @@
 "use strict"
-
+"use strict"
 class Queue {
-  constructor() {}
+  constructor() {
+    this.isKosong = true
+    this.max_value = 10
+    this.tampungStack = []
+  }
+
+  push(x) {
+    if(this.isKosong){
+    this.tampungStack.push(x)
+  }else return this.isFull()
+  }
+
+  pop() {
+    if(this.isKosong){
+    this.tampungStack.shift()
+  }else return this.isFull()
+  }
+
+  peek() {
+    console.log(this.tampungStack);
+  }
+
+  isEmpty() {
+    if(this.tampungStack.length < this.max_value)
+    {
+      this.isKosong = true
+    }else{
+      this.isKosong = false
+      return this.tampungStack
+    }
+  }
+
+  isFull()  {
+      if(this.tampungStack.length >= this.max_value)
+      {
+        console.log('The stack is already full !');
+        console.log('Your stack length already reach max which is ');
+      }
+  }
 }
+
+var stack = new Queue()
+stack.isEmpty()
+stack.push('javascript')
+stack.push('is')
+stack.push('cool')
+stack.push('javascript')
+stack.pop()//pop first in first out
+stack.peek()
+stack.isFull()
--- stack.js
+++ stack.js
@@ -1,19 +1,52 @@
 "use strict"
-
 class Stack {
   constructor() {
+    this.isKosong = true
+    this.max_value = 10
+    this.tampungStack = []
   }
 
   push(x) {
+    if(this.isKosong){
+    this.tampungStack.push(x)
+  }else return this.isFull()
   }
 
   pop() {
+    if(this.isKosong){
+    this.tampungStack.pop()
+  }else return this.isFull()
   }
 
   peek() {
-
+    console.log(this.tampungStack);
   }
 
   isEmpty() {
+    if(this.tampungStack.length < this.max_value)
+    {
+      this.isKosong = true
+    }else{
+      this.isKosong = false
+      return this.tampungStack
+    }
+  }
+
+  isFull()  {
+      if(this.tampungStack.length >= this.max_value)
+      {
+        console.log('The stack is already full !');
+        console.log('Your stack length already reach max which is ');
+      }
   }
 }
+
+var stack = new Stack()
+stack.isEmpty()
+stack.push('javascript')
+stack.push('is')
+stack.push('cool')
+stack.push('javascript')
+stack.pop()//pop last in first out
+stack.peek()
+stack.isFull()
