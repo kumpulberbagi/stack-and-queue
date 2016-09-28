ad2b67ddcdf9368df4a29420cdc87bbfcc3e2d58
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-09-28 15:00:01 +0700

    stack queueueu done

--- queue.js
+++ queue.js
@@ -1,5 +1,65 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor(property, limit) {
+    this.name = property
+    this.storage = []
+    this.MAX_SIZE = limit || 10
+  }
+
+  push(x) {
+    if(this.storage.length >= this.MAX_SIZE){
+      console.log(`Queue ${this.name} are full. Push failed.`);
+    } else {
+      this.storage.reverse()
+      this.storage.push(x)
+      this.storage.reverse()
+    }
+  }
+
+  pop() {
+    if (this.storage == []){
+      console.log(`Queue ${this.name} are already Empty !`);
+    } else {
+      this.storage.pop()
+    }
+  }
+
+  peek() {
+    console.log(`Latest data on ${this.name} : ` + this.storage[0]);
+  }
+
+  isEmpty() {
+    if(this.storage == []){
+      console.log(`Queue ${this.name} are Empty !`);
+    } else {
+      console.log(`Queue ${this.name} are not Empty !`);
+    }
+  }
+
+  isFull(){
+    if(this.storage.length >= this.MAX_SIZE){
+      console.log(`Queue ${this.name} are full. Push failed.`);
+    }
+  }
 }
+
+var fox = new Queue("Fox")
+fox.push("test A")
+fox.push("test B")
+fox.push("test C")
+fox.push("test D")
+fox.push("test E")
+fox.push("test F")
+fox.push("test G")
+fox.push("test H")
+fox.push("test I")
+fox.push("test J")
+console.log(fox);
+fox.push("test K")
+fox.push("test L")
+console.log(fox);
+fox.pop()
+console.log(fox);
+fox.peek()
+fox.isEmpty()
--- stack.js
+++ stack.js
@@ -1,19 +1,63 @@
 "use strict"
 
 class Stack {
-  constructor() {
+  constructor(property, limit) {
+    this.name = property
+    this.storage = []
+    this.MAX_SIZE = limit || 10
   }
 
   push(x) {
+    if(this.storage.length >= this.MAX_SIZE){
+      console.log(`Stack ${this.name} are full. Push failed.`);
+    } else {
+      this.storage.push(x)
+    }
   }
 
   pop() {
+    if (this.storage == []){
+      console.log(`Stack ${this.name} are already Empty !`);
+    } else {
+      this.storage.pop()
+    }
   }
 
   peek() {
-
+    console.log(`Latest data on ${this.name} : ` + this.storage[this.storage.length-1]);
   }
 
   isEmpty() {
+    if(this.storage == []){
+      console.log(`Stack ${this.name} are Empty !`);
+    } else {
+      console.log(`Stack ${this.name} are not Empty !`);
+    }
+  }
+
+  isFull(){
+    if(this.storage.length >= this.MAX_SIZE){
+      console.log(`Stack ${this.name} are full. Push failed.`);
+    }
   }
 }
+
+var toni = new Stack("Toni")
+toni.push("test A")
+toni.push("test B")
+toni.push("test C")
+toni.push("test D")
+toni.push("test E")
+toni.push("test F")
+toni.push("test G")
+toni.push("test H")
+toni.push("test I")
+toni.push("test J")
+console.log(toni);
+toni.push("test K")
+toni.push("test L")
+console.log(toni);
+toni.pop()
+console.log(toni);
+toni.peek()
+toni.isEmpty()
