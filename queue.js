8e4eba2da1a7c3331e2753e529e28c8c80ba3801
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-09-28 19:55:20 +0700

    stack & queue

--- queue.js
+++ queue.js
@@ -1,5 +1,40 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+	constructor() {
+		this._arr = [];
+	}
+	
+	push(x) {
+		this._arr.push(x);
+		return this;
+	}
+	
+	pop() {
+		this._arr.shift();
+		return this;
+	}
+	
+	isEmpty() {
+		if(this._arr.length == 0){
+			return "empty";
+		}else{
+			return "Ada isi";
+		}
+	}
+	
+	result(){
+		return this._arr; 
+	}
 }
+
+let queue = new Queue();
+
+console.log('The Stack Is Empty?' + queue.isEmpty());
+
+console.log(queue.push("Javascript"));
+console.log(queue.push("is so"));
+console.log(queue.pop());
+console.log(queue.push("cool"));
+console.log(queue.pop());
+//console.log(my_stack.result());
--- stack.js
+++ stack.js
@@ -1,19 +1,72 @@
 "use strict"
-
+const MAX_SIZE = 10;
 class Stack {
-  constructor() {
-  }
-
-  push(x) {
-  }
-
-  pop() {
-  }
-
-  peek() {
-
-  }
-
-  isEmpty() {
-  }
+	constructor() {
+		this._arr = [];
+		this._peek;
+	}
+	
+	push(x) {
+		//console.log(this.isFull());
+		if(this.isFull() == 1){
+			console.log("Array Is Full");
+			return this;	
+		}else{
+			this._arr.push(x);
+			return this;	
+		}
+	}
+	
+	pop() {
+		this._arr.pop();
+		return this;
+	}
+	
+	peek() {
+		this._peek = this._arr[this._arr.length-1];
+		return this._peek;
+	}
+	
+	isEmpty() {
+		if(this._arr.length == 0){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	
+	isFull(){
+		if(this._arr.length > MAX_SIZE){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	
+	result(){
+		return this._arr; 
+	}
 }
+
+let my_stack = new Stack();
+
+console.log('The Stack Is Empty?' + my_stack.isEmpty());
+
+console.log(my_stack.push("Javascript"));
+//console.log(my_stack.pop());
+console.log(my_stack.push("is so"));
+console.log(my_stack.push("cool"));
+console.log(my_stack.push("4").push("5"));
+console.log(my_stack.push("6"));
+console.log(my_stack.push("7"));
+console.log(my_stack.push("8"));
+console.log(my_stack.push("9"));
+console.log(my_stack.push("10"));
+console.log(my_stack.push("11"));
+
+console.log(my_stack.push("ditolak").push("ditolak"));
+
+console.log(my_stack.peek());
+//console.log('The Stack Is Empty?' + my_stack.isEmpty());
+//console.log(my_stack.isFull());
+console.log(my_stack.result());
