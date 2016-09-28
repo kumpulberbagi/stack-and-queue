f969c566c61b908a196cf012ddae5a93e2e5617d
Septian A Tama <tama@tamatamvan.web.id>
2016-09-28 18:34:35 +0700

    challenge stack  from stack and queue
    challenge stack  from stack and queue, queue menyusul

--- stack.js
+++ stack.js
@@ -1,19 +1,58 @@
 "use strict"
 
 class Stack {
-  constructor() {
+  constructor(property, limit) {
+    this._property = property;
+    this._limit = limit || 10;
+    this._datas = []
   }
 
   push(x) {
+    if (this._datas.length <= this._limit) {
+      this._datas.push(x);
+    }
+    else {
+      console.log('This stack is full! Cant push anymore!');
+    }
+    return "You've just push " + "'" + x + "'" + " to datas";
   }
 
   pop() {
+    if (this._datas === []) {
+      console.log("There's no data left, nothing to delete!");
+    }
+    else {
+      this._datas.splice(this._datas.length-1, 1);
+    }
+    return "You've just pop the datas";
   }
 
   peek() {
-
+    if (this._datas === []) {
+      console.log("There's no data be displayed!");
+    }
+    else {
+      return this._datas[this._datas.length-1];
+    }
   }
 
   isEmpty() {
+    if (this._datas === []) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
+
+var my_stack = new Stack("Hello!");
+
+console.log(my_stack.push("World!"));
+console.log(my_stack.push("Eat"));
+console.log(my_stack.push("Pray"));
+console.log(my_stack.push("Code"));
+
+console.log(my_stack.peek());
+
+console.log(my_stack.pop());
+console.log(my_stack.peek());
