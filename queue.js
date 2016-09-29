f6d0083c546d74a0523d8fec8d68c368d11fdbca
YONI SETIAWAN <yonisetiawan1990@gmail.com>
2016-09-29 08:40:27 +0700

    stack and queue done

--- queue.js
+++ queue.js
@@ -1,5 +1,61 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+     this.str = [];
+     this.top = 0;
+     this.max_size = 4;
+  }
+
+  push(x) {
+    this.str.push(x)
+    this.top +=1
+  }
+
+  shift() {
+    return this.str.shift();
+  }
+
+  // peek() {
+  //     return this.str.shift();
+  // }
+
+
+  isEmpty() {
+    return this.str;
+  }
+
+  isFull(){
+    var top = this.str.length
+    if(this.top === this.max_size){
+      return "Penuh"
+    }else{
+      return "Belum"
+    }
+
+  }
+
 }
+
+
+let my_queue = new Queue()
+
+console.log(`the stack is empty? ${my_queue.isEmpty()}`)
+
+my_queue.push("JavaScript")
+my_queue.push("is Just so")
+my_queue.push("cool")
+my_queue.push(15)
+
+console.log(my_queue.isFull())
+
+//console.log(my_queue.peek())
+
+console.log(my_queue.shift())
+console.log(my_queue.shift())
+console.log(my_queue.shift())
+console.log(my_queue.shift())
+
+console.log(`The stack is empty? ${my_queue.isEmpty()}`)
+
+console.log(my_queue.shift())
--- stack.js
+++ stack.js
@@ -2,18 +2,60 @@
 
 class Stack {
   constructor() {
+     this.str = [];
+     this.top = 0;
+     this.max_size = 4;
   }
 
   push(x) {
+    this.str.push(x)
+    this.top +=1
   }
 
   pop() {
+    return this.str.pop();
   }
 
   peek() {
-
+      return this.str.shift();
   }
+
 
   isEmpty() {
+    return this.str;
   }
+
+  isFull(){
+    var top = this.str.length
+    if(this.top === this.max_size){
+      return "Penuh"
+    }else{
+      return "Belum"
+    }
+
+  }
+
 }
+
+
+let my_stack = new Stack()
+
+console.log(`the stack is empty? ${my_stack.isEmpty()}`)
+
+my_stack.push("JavaScript")
+my_stack.push("is Just so")
+my_stack.push("cool")
+my_stack.push(15)
+
+console.log(my_stack.isFull())
+
+console.log(my_stack.peek())
+
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
+
+console.log(my_stack.pop())
