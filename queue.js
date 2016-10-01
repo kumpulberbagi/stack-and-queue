21975b2c0000b70401721b33cdc979173a44c720
Elkana Lumbantobing <email": null>
2016-10-01 15:17:42 +0700

    stack and queue

--- queue.js
+++ queue.js
@@ -1,5 +1,54 @@
 "use strict"
 
 class Queue {
-  constructor() {}
-}
+    constructor() {
+      this.wadah = []
+      this.MAX_SIZE = 5
+      }
+
+    masuk(x) {
+      if(this.wadah.length === 5){
+        this.isFull();
+      }
+      else{
+        this.wadah.unshift(x);
+      }
+    }
+
+    keluar() {
+      this.wadah.pop();
+    }
+
+    peek() {
+      let top = this.wadah[this.wadah.length-1]
+      return top
+    }
+
+    isEmpty() {
+      if(this.wadah.length===0){
+        return "Wadah kosong gan.."
+      }
+      else {
+        return "Wadah tidak kosong.."
+      }
+    }
+
+    isi() {
+      return "Ada "+this.wadah.length+ " buah"
+    }
+
+    isFull(){
+      console.log("Sudah Penuhh!!");
+    }
+  }
+
+  var buah = new Queue();
+  buah.masuk("Apel");
+  buah.masuk("Jambu");
+  buah.masuk("Pisang");
+  buah.masuk("Mangga");
+  buah.masuk("Anggur");
+
+  console.log(buah.isi());
+  console.log(buah.peek());
+  console.log(buah.isEmpty());
--- stack.js
+++ stack.js
@@ -2,18 +2,54 @@
 
 class Stack {
   constructor() {
-  }
+    this.wadah = []
+    this.MAX_SIZE = 5
+    }
 
   push(x) {
+    if(this.wadah.length === 5){
+      this.isFull();
+    }
+    else{
+      this.wadah.push(x);
+    }
   }
 
   pop() {
+    this.wadah.pop();
+  }
+
+  isi() {
+    return "Ada "+this.wadah.length+ " buah"
   }
 
   peek() {
-
+    let top = this.wadah[this.wadah.length-1]
+    return top
   }
 
   isEmpty() {
+    if(this.wadah.length===0){
+      return "Wadah kosong gan.."
+    }
+    else {
+      return "Wadah tidak kosong.."
+    }
+  }
+
+  isFull(){
+    console.log("Sudah Penuhh!!");
   }
 }
+
+var buah = new Stack();
+buah.push("Apel");
+buah.push("Jambu");
+buah.push("Pisang");
+buah.push("Mangga");
+buah.push("Anggur");
+buah.push("Semangka");
+
+console.log(buah.wadah.length);
+console.log(buah.peek());
+console.log(buah.isEmpty());
